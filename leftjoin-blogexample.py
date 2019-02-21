5010d81390b22c9e5097ed2feb028760ab02d076
Hoc Leng Chung <hoc.leng.chung@devoteam.com>
2019-02-21 14:00:43 +0100

    rev leftjoin code

--- leftjoin-blogexample.py
+++ leftjoin-blogexample.py
@@ -1,12 +1,9 @@
 from apache_beam.options.pipeline_options import PipelineOptions
 import apache_beam as beam
 
-
 class LeftJoin(beam.PTransform):
-
-
-    """This PTransform performs a left join given source_pipeline_name, source_data, join_pipeline_name, join_data, common_key constructors"""
-
+    """This PTransform performs a left join given source_pipeline_name, source_data,
+     join_pipeline_name, join_data, common_key constructors"""
 
 def __init__(self, source_pipeline_name, source_data, join_pipeline_name, join_data,  \common_key):
     self.join_pipeline_name = join_pipeline_name
@@ -14,7 +11,6 @@ def __init__(self, source_pipeline_name, source_data, join_pipeline_name, join_d
     self.source_pipeline_name = source_pipeline_name
     self.join_data = join_data
     self.common_key = common_key
-
 
 def expand(self, pcolls):
     def _format_as_common_key_tuple(data_dict, common_key):
@@ -29,7 +25,6 @@ def expand(self, pcolls):
                                                self.source_pipeline_name,
                                                self.join_pipeline_name)
             )
-
 
 class UnnestCoGrouped(beam.DoFn):
     """This DoFn class unnests the CogroupBykey output and emits """
