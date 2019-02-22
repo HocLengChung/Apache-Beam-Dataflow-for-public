ae520d297a836a662e6a9f376ade3c3ee8e82d35
Hoc Leng Chung <hoc.leng.chung@devoteam.com>
2019-02-22 08:53:23 +0100

    Added comment

--- leftjoin-blogexample.py
+++ leftjoin-blogexample.py
@@ -17,7 +17,8 @@ def __init__(self, source_pipeline_name, source_data, join_pipeline_name, join_d
 def expand(self, pcolls):
     def _format_as_common_key_tuple(data_dict, common_key):
         return data_dict[common_key], data_dict
-
+    """This part here below starts with a python dictionary comprehension in case you 
+    get lost in what is happening :-)"""
     return ({pipeline_name: pcoll | 'Convert to ({0}, object) for {1}'
             .format(self.common_key, pipeline_name)
                             >> beam.Map(_format_as_common_key_tuple, self.common_key)
